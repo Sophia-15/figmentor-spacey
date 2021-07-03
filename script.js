e559d3b136aa0832301e9e7f5019cde0ecfd5de5
Sophia Gallindo <deletrasopa@gmail.com>
2021-07-03 15:45:20 -0300

    fixing the lines

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-const sr = ScrollReveal({
+const sr = ScrollReveal()
+document.querySelector('#garantir').addEventListener('click', event => {
+  event.preventDefault()
+})
+
+
+sr.reveal('.informative-card1', {
   origin: 'top',
   distance: '50px',
   duration: 2000,
   reset: true
 })
 
-document.querySelector('#garantir').addEventListener('click', event => {
-  event.preventDefault()
+sr.reveal('.informative-card2', {
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
 })
 
+sr.reveal('.informative-card3', {
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+})
 
-sr.reveal('.informative-card1')
-sr.reveal('.informative-card2')
-sr.reveal('.informative-card3')
-sr.reveal('.mars')
-sr.reveal('button')
-sr.reveal('.rocket-img')
+sr.reveal('.mars', {
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+})
+
+sr.reveal('.mars-text', {
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+})
+
+sr.reveal('.learn-more', {
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+})
+
+sr.reveal('.rocket-img', {
+  origin: 'bottom',
+  distance: '500px',
+  duration: 2000,
+  reset: true
+})
